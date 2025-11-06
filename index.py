39df72bd54d55d3d946438252bc95e466e1f9f95
saimnabeel <Saimnabeeliqbal@gmail.com>
2025-11-06 17:41:36 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+first_name = "Saim"
+last_name = "Nabeel"
+full_name="Saim nabeel"*3
+print("Hello " + first_name + " " + last_name)
+print(full_name)
+word="Python"
+print(word[0])
+print(word[1])
+print(word[2])
+print(word[3])
+print(word[4])
+print(word[5])
+print(word[6])
